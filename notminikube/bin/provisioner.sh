33f955ded6579dc799562ed51ff5b030613f5826
Angel Alonso Fonseca <alonsofonseca.angel@gmail.com>
2018-10-11 21:18:39 +0200

    kube master up and running

--- notminikube/bin/provisioner.sh
+++ notminikube/bin/provisioner.sh
@@ -2,11 +2,15 @@
 #TODO:
 # test kubeadm from scratch
 # change 10.0.2.15 on sed to use MASTER1IP instead
-THISHOME="/home/vagrant"
+VAGRANTHOME="/home/vagrant"
 
 export DEBIAN_FRONTEND=noninteractive
+CLUSTERCIDR="10.30.0.0/24"
 HAPROXYIP="10.0.2.15"
 MASTER1IP="10.0.2.15"
+
+K8SV="1.10.6"
+CALICOV="3.0"
 
 update() {
   echo "################## update "
@@ -41,23 +45,23 @@ get_kubectl() {
 get_haproxy() {
   echo "################## haproxy "
   sudo apt-get install -y haproxy
-  sudo sed -i 's/ww.ww.ww.ww/10.0.2.15/g' $THISHOME/files/haproxy.cfg
-  sudo sed 's/xx.xx.xx.xx/10.0.2.15/g' $THISHOME/files/haproxy.cfg > /etc/haproxy/haproxy.cfg
+  sudo sed -i 's/ww.ww.ww.ww/10.0.2.15/g' $VAGRANTHOME/files/haproxy.cfg
+  sudo sed 's/xx.xx.xx.xx/10.0.2.15/g' $VAGRANTHOME/files/haproxy.cfg > /etc/haproxy/haproxy.cfg
   sudo systemctl restart haproxy
 }
 
 tls() {
   echo "################## TLS "
   echo "## Certificate Authority"
-  cfssl gencert -initca $THISHOME/files/ca-csr.json | cfssljson -bare ca
+  cfssl gencert -initca $VAGRANTHOME/files/ca-csr.json | cfssljson -bare ca
   ls -la 
   echo "## Etcd Cluster"
   cfssl gencert \
     -ca=ca.pem \
     -ca-key=ca-key.pem \
-    -config=$THISHOME/files/ca-config.json \
+    -config=$VAGRANTHOME/files/ca-config.json \
     -hostname=$MASTER1IP,127.0.0.1,kubernetes.default \
-    -profile=kubernetes $THISHOME/files/kubernetes-csr.json | \
+    -profile=kubernetes $VAGRANTHOME/files/kubernetes-csr.json | \
     cfssljson -bare kubernetes
   ls -la
   #scp ca.pem kubernetes.pem kubernetes-key.pem sguyennet@10.10.40.90:~
@@ -89,12 +93,12 @@ get_kubethings() {
 get_etcd() {
   echo "################## Etcd"
   sudo mkdir -p /etc/etcd /var/lib/etcd
-  sudo mv $THISHOME/ca.pem $THISHOME/kubernetes.pem $THISHOME/kubernetes-key.pem /etc/etcd
+  sudo mv $VAGRANTHOME/ca.pem $VAGRANTHOME/kubernetes.pem $VAGRANTHOME/kubernetes-key.pem /etc/etcd
   wget --quiet https://github.com/coreos/etcd/releases/download/v3.3.9/etcd-v3.3.9-linux-amd64.tar.gz 2>/dev/null
   sleep 1
   tar xvzf etcd-v3.3.9-linux-amd64.tar.gz
   sudo mv etcd-v3.3.9-linux-amd64/etcd* /usr/local/bin/
-  sudo sed 's/xx.xx.xx.xx/10.0.2.15/g' $THISHOME/files/etcd.service > /etc/systemd/system/etcd.service
+  sudo sed 's/xx.xx.xx.xx/10.0.2.15/g' $VAGRANTHOME/files/etcd.service > /etc/systemd/system/etcd.service
   sudo systemctl daemon-reload
   sudo systemctl enable etcd
   sudo systemctl start etcd
@@ -104,29 +108,32 @@ get_etcd() {
 
 init_master() {
   echo "################## Initializing Master"
-  sudo sed -i 's/ww.ww.ww.ww/10.0.2.15/g' $THISHOME/files/kubeadm_config.yaml 
-  sudo sed -i 's/xx.xx.xx.xx/10.0.2.15/g' $THISHOME/files/kubeadm_config.yaml 
+  sudo sed -i 's/ww.ww.ww.ww/10.0.2.15/g' $VAGRANTHOME/files/kubeadm_config.yaml 
+  sudo sed -i 's/xx.xx.xx.xx/10.0.2.15/g' $VAGRANTHOME/files/kubeadm_config.yaml 
   sudo systemctl stop etcd
   sudo rm -rf /var/lib/etcd/member
-  sudo kubeadm init --config=$THISHOME/files/kubeadm_config.yaml
+  sudo kubeadm init --config=$VAGRANTHOME/files/kubeadm_config.yaml
   #sudo scp -r /etc/kubernetes/pki sguyennet@10.10.40.91:~
   su -l vagrant -c 'mkdir -p $HOME/.kube'
   su -l vagrant -c 'sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config'
   su -l vagrant -c 'sudo chown $(id -u):$(id -g) $HOME/.kube/config'
-  echo "DID SOME OTHER THINGS...see comments on script"
-  # modify endpoints to use port 6444 of haproxy
-  # install calico
-  #   kubectl apply -f https://docs.projectcalico.org/v3.1/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
-  #   kubectl apply -f https://docs.projectcalico.org/v3.1/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml
-  #...some other things
-
+  echo "downloading Calico"
+  wget --quiet https://docs.projectcalico.org/v${CALICOV}/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
+  wget --quiet https://docs.projectcalico.org/v${CALICOV}/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml
+  su -l vagrant -c 'sudo chown $(id -u):$(id -g) $HOME/rbac-kdd.yaml'
+  su -l vagrant -c 'sudo chown $(id -u):$(id -g) $HOME/calico.yaml'
+  sed -i 's#192.168.0.0/16#10.30.0.0/24#g' $VAGRANTHOME/calico.yaml
+  echo "applying Calico"
+  su -l vagrant -c 'kubectl apply -f $HOME/rbac-kdd.yaml'
+  su -l vagrant -c 'kubectl apply -f $HOME/calico.yaml'
+  rm $VAGRANTHOME/rbac-kdd.yaml
+  rm $VAGRANTHOME/calico.yaml
 }
-
 testfile(){
   echo "#########################################################"
   echo "######### TEST"
-  chmod +x $THISHOME/files/test.sh
-  $THISHOME/files/test.sh
+  chmod +x $VAGRANTHOME/files/test.sh
+  $VAGRANTHOME/files/test.sh
 }
 
 update
